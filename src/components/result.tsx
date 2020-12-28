865fc5be2bdc7d8c2bd735d02d1f86a38f067602
paranoidCrocodile <cutefishthatgrewlegs@gmail.com>
2020-12-28 14:57:48 +0800

    src/components/result更改

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
+import styled from "styled-components";
 import { PromiseObj } from "../pages";
 
 interface ResultProps {
   response: PromiseObj;
 }
 
+const H1 = styled.h1`
+  color: black;
+  text-align: center;
+  font-size: 1.5rem;
+  margin: 1rem 0 0.5rem 0;
+`;
+
+const Img = styled.img``;
+
 const Result = ({ response }: ResultProps): React.ReactElement => {
-  useEffect(() => console.log(response), []);
-  return <div></div>;
+  const { title, img } = response;
+  return (
+    <div>
+      <H1>{title}</H1>
+      <Img src={img} />
+    </div>
+  );
 };
 
 export default Result;
