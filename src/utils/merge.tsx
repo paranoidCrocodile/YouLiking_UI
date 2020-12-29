464b55268dd2c3d0b1116dda97498e95ad4f2414
paranoidCrocodile <cutefishthatgrewlegs@gmail.com>
2020-12-30 02:22:54 +0800

    Updates

--- src/utils/merge.tsx
+++
@@ -1,7 +0,0 @@
-import { StateObj } from "../pages/index";
-
-const merge = (oldObj: StateObj, newObj: Record<string, unknown>): StateObj => {
-  return { ...oldObj, ...newObj };
-};
-
-export default merge;
